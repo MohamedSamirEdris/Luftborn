c6d6b1034c450849388bf2cf0e9998c5ba6e48fd
mohamedsamir <mohamedsamiredries@outlook.com>
2023-02-18 08:45:03 +0200

    Some modification

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,21 @@
-import React from "react";
-import buttonImg from "../assets/icon-ellipsis.svg";
+import React from 'react';
+import buttonImg from '../assets/icon-ellipsis.svg';
 
-function Card({ product }) {
-  const { category, rating, description, id, image, price, title } = product;
-  const { rate, count } = rating;
+function Card({product}) {
+  const {category, rating, description, image, price, title} = product;
+  const {count} = rating;
 
   return (
-    <div className={`card ${title.split(" ").join("-").toLowerCase()}`}>
-      <div className="card__banner"></div>
+    <div className={`card ${title.split(' ').join('-').toLowerCase()}`}>
+      <div className="card__banner" />
       <div className="card__body">
         <h2 className="title">{category}</h2>
         <button className="button-setting">
           <img src={buttonImg} alt="other settings" />
         </button>
         <span className="price">{price}$</span>
-        <p className="title">{title}</p>
-        <h3 className="current-time">{count} In Store</h3>
-        <h3 className="previous-time"></h3>
+        <p className="desc">{description}</p>
+        <h3 className="count">{count} In Store</h3>
       </div>
     </div>
   );
